8ab0e2b1a90b8ca0f25b9fe7ebd4af83b8820449
Osmani Abderrahmane <abderrahmaneosmani2@gmail.com>
2024-10-08 10:53:16 +0100

    setup mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/management')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  onModuleInit() {
+    mongoose.connection.on('connected', () => {
+      Logger.debug('Connected to MongoDB');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      Logger.error('MongoDB connection error:', err);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      Logger.debug('Disconnected from MongoDB');
+    });
+  }
+}
